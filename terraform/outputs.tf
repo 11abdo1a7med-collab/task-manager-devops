63029713231c2720a061676407d93da7e7eb64d8
abdo <11abdo1a7med@gmail.com>
2026-09-26 15:29:47 +0300

    infra: add core terraform configuration files

---
+++ terraform/outputs.tf
@@ -0,0 +1,34 @@
+output "cluster_name" {
+  description = "EKS cluster name"
+  value       = aws_eks_cluster.main.name
+}
+
+output "cluster_endpoint" {
+  description = "EKS API endpoint"
+  value       = aws_eks_cluster.main.endpoint
+}
+
+output "cluster_arn" {
+  description = "EKS cluster ARN"
+  value       = aws_eks_cluster.main.arn
+}
+
+output "vpc_id" {
+  description = "VPC ID"
+  value       = aws_vpc.main.id
+}
+
+output "private_subnet_ids" {
+  description = "Private subnet IDs"
+  value       = aws_subnet.private[*].id
+}
+
+output "public_subnet_ids" {
+  description = "Public subnet IDs"
+  value       = aws_subnet.public[*].id
+}
+
+output "node_group_name" {
+  description = "EKS managed node group name"
+  value       = aws_eks_node_group.main.node_group_name
+}
